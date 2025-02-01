411e8d1d1086075e4413619c9dab6b12a2cfe431
Aleena Saju <sajualeena1@gmail.com>
2025-02-01 20:03:12 +0530

    update port name

--- app.js
+++ app.js
@@ -64,8 +64,10 @@ app.use("/admin", adminRouter);
 
 // Start the server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
+
